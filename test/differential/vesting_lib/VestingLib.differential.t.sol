9387836c35133c7613fefd9c5c43a7de0fb74c90
marija-mijailovic <marija.mijailovic@mvpworkshop.co>
2023-11-09 15:22:21 +0100

    Differential test for the vesting amount calculation

---
+++ test/differential/vesting_lib/VestingLib.differential.t.sol
@@ -0,0 +1,63 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+pragma solidity ^0.8.0;
+
+import "forge-std/Test.sol";
+import "forge-std/console.sol";
+import {VestingLib} from "../../../contracts/utils/VestingLib.sol";
+import {Strings} from "@openzeppelin/contracts/utils/Strings.sol";
+import {SafeCast} from "../../../contracts/vendor/openzeppelin/v4.9.0/utils/math/SafeCast.sol";
+
+contract VestingLibDifferentialTest is VestingLib, Test {
+  using Strings for uint256;
+ 
+  address beneficiary; 
+  uint256 amount;
+  uint256 startTimestampRange;
+  uint48 startTimestamp;
+  uint32 cliffDuration;
+  uint32 vestingDuration;
+  VestingPosition vestingPosition;
+   
+  function setUp() public {
+    amount = bound(amount, 10 ** 18, 133_000_000 * 10 ** 18);
+    beneficiary = makeAddr("beneficiary");
+    startTimestampRange = bound(startTimestampRange, 1704067200, 1893456000); // @dev [01 January 2024 - 01 January 2030]
+    startTimestamp = SafeCast.toUint48(startTimestampRange); 
+    cliffDuration = SafeCast.toUint32(15638400); // @dev 6 month Wednesday, 1 July 1970 00:00:00
+    vestingDuration = SafeCast.toUint32(44582400); // @dev 18month Tuesday, 1 June 1971 00:00:00
+
+    vestingPosition = createVestingPosition(amount, beneficiary, startTimestamp, cliffDuration, vestingDuration);
+  }
+
+  function ffi_vestedAmount(uint256 blockTimestamp) private returns (uint256 vestedAmountRust) {
+    string[] memory inputs = new string[](10);
+    inputs[0] = "cargo";
+    inputs[1] = "run";
+    inputs[2] = "--quiet";
+    inputs[3] = "--manifest-path";
+    inputs[4] = "test/differential/vesting_lib/Cargo.toml";
+    inputs[5] = amount.toString();
+    inputs[6] = blockTimestamp.toString();
+    inputs[7] = uint256(startTimestamp).toString();
+    inputs[8] = uint256(startTimestamp + SafeCast.toUint48(cliffDuration)).toString();
+    inputs[9] = uint256(cliffDuration + vestingDuration).toString(); // @dev 60220800 - 2 years
+       
+    bytes memory result = vm.ffi(inputs);    
+
+    vestedAmountRust = abi.decode(result, (uint256));
+  }
+
+  function test_vestedAmount(uint256 blockTimestamp) external {
+    uint48 maxClaimTime = startTimestamp + (SafeCast.toUint48(vestingPosition.totalDuration)*12); // @dev 30 years for claiming
+    blockTimestamp = bound(blockTimestamp, vestingPosition.startTimestamp, maxClaimTime);
+    vm.warp(blockTimestamp);
+
+    uint256 vestedAmountRust = ffi_vestedAmount(blockTimestamp);
+    uint256 vestedAmountSol = releasableAmount(0);
+
+    console.logUint(vestedAmountRust);
+    console.logUint(vestedAmountSol);
+
+    assertEq(vestedAmountRust, vestedAmountSol);
+  }
+}
---
+++ test/differential/vesting_lib/src/main.rs
@@ -0,0 +1,47 @@
+use alloy_primitives::{U256, U32, U64};
+use std::env;
+
+fn vested_amount(
+    total_vested_amount: U256,
+    block_timestamp: U64,
+    start_timestamp: U64,
+    cliff_timestamp: U64,
+    total_duration: U32,
+) -> U256 {
+    if block_timestamp < cliff_timestamp {
+        return U256::from(0);
+    } else if block_timestamp >= (start_timestamp + U64::from(total_duration)) {
+        return total_vested_amount;
+    } else {
+        return total_vested_amount * U256::from(block_timestamp - start_timestamp)
+            / U256::from(total_duration);
+    }
+}
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let total_vested_amount: U256 = args[1]
+        .parse::<U256>()
+        .expect("Can't parse total vested amount");
+    let block_timestamp: U64 = args[2].parse::<U64>().expect("Can't parse block timestamp");
+    let start_timestamp: U64 = args[3].parse::<U64>().expect("Can't parse start timestamp");
+    let cliff_timestamp: U64 = args[4]
+        .parse::<U64>()
+        .expect("Can't parse cliff timestampt");
+    let total_duration: U32 = args[5].parse::<U32>().expect("Can't parse total duration");
+
+    let vested_amount = vested_amount(
+        total_vested_amount,
+        block_timestamp,
+        start_timestamp,
+        cliff_timestamp,
+        total_duration,
+    );
+
+    if vested_amount == U256::from(0) {
+        // NOTE - This is needed as the ffi returns 0x307830 as result and then it decode as non zero value
+        print!("0x{number:0>64x}", number = 0);
+    } else {
+        print!("{:#x}", vested_amount);
+    }
+}
