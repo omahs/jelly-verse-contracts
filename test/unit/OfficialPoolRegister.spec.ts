0d1aa8f8f560efcb0b716ee3adc8c63bc09bd4dc
markoMVP85 <marko.jovanovic@mvpworkshop.co>
2023-11-20 07:04:42 +0100

    JELLY-729 refactor test

--- test/unit/OfficialPoolRegister.spec.ts
+++ test/unit/OfficialPoolRegister.spec.ts
@@ -1,3 +1,4 @@
+import { loadFixture } from '@nomicfoundation/hardhat-network-helpers';
 import { expect } from 'chai';
 import { Signer } from 'ethers';
 import { ethers } from 'hardhat';
@@ -5,23 +6,30 @@ import { ethers } from 'hardhat';
 // @notice -- I did not test ownable cuz it is not necessary and it is community approved
 
 describe('OfficialPoolsRegister', function () {
+  async function deployUnitFixture() {
+    const [deployer, user, ...otherSigners] = await ethers.getSigners();
+
+    const OfficialPoolsRegisterFactory = await ethers.getContractFactory('OfficialPoolsRegister');
+    const officialPoolsRegister = await OfficialPoolsRegisterFactory.deploy(
+        deployer.address,
+        user.address
+    );
+
+    return { officialPoolsRegister, deployer, user, otherSigners };
+  }
+
   let officialPoolsRegister: any;
   let owner: Signer;
   let pendingOwner: Signer;
   let otherSigners: Signer[];
-  const poolId = "100";
+  const poolId = "0x7f65ce7eed9983ba6973da773ca9d574f285a24c000200000000000000000000";
 
   beforeEach(async function () {
-    [owner, pendingOwner, ...otherSigners] = await ethers.getSigners();
-
-    const OfficialPoolsRegisterFactory = await ethers.getContractFactory(
-      "OfficialPoolsRegister",
-    );
-    officialPoolsRegister = await OfficialPoolsRegisterFactory.deploy(
-      await owner.getAddress(),
-      await pendingOwner.getAddress(),
-    );
-    await officialPoolsRegister.deployed();
+    const fixture = await loadFixture(deployUnitFixture);
+    officialPoolsRegister = fixture.officialPoolsRegister;
+    owner = fixture.deployer;
+    pendingOwner = fixture.user;
+    otherSigners = fixture.otherSigners;
   });
 
   describe("Check for initial state after deployment", async function () {
@@ -39,63 +47,76 @@ describe('OfficialPoolsRegister', function () {
     });
   });
 
-  describe("registerOfficialPool", async function () {
-    it("should create pool successfully", async () => {
-      expect(officialPoolsRegister.registerOfficialPool(poolId))
-        .to.emit(officialPoolsRegister, "OfficialPoolRegistered")
-        .withArgs(await owner.getAddress(), poolId);
+  describe("#registerOfficialPool", async function () {
+    describe("success", async () => {
+      it("should register pool successfully", async () => {
+        expect(officialPoolsRegister.registerOfficialPool(poolId))
+          .to.emit(officialPoolsRegister, "OfficialPoolRegistered")
+          .withArgs(await owner.getAddress(), poolId);
+  
+        const officialPoolsIds =
+          await officialPoolsRegister.getAllOfficialPools();
+        expect(officialPoolsIds[0], poolId);
+      });
+    })
 
-      const officialPoolsIds =
-        await officialPoolsRegister.getAllOfficialPools();
-      expect(officialPoolsIds[0], poolId);
-    });
-
-    it("should revert on create the same pool", async () => {
-      expect(
-        officialPoolsRegister.registerOfficialPool(poolId),
-      ).to.be.revertedWithCustomError(
-        officialPoolsRegister,
-        "OfficialPoolsRegister_InvalidPool",
-      );
-    });
-
-    it("should revert when registering a pool by a non-owner", async () => {
-      expect(
-        officialPoolsRegister
-          .connect(otherSigners[0])
-          .registerOfficialPool(poolId),
-      ).to.be.revertedWithCustomError(
-        officialPoolsRegister,
-        "Ownable__CallerIsNotOwner",
-      );
-    });
+    describe("failure", async () => {
+      it("should revert on create the same pool", async () => {
+        expect(
+          officialPoolsRegister.registerOfficialPool(poolId),
+        ).to.be.revertedWithCustomError(
+          officialPoolsRegister,
+          "OfficialPoolsRegister_InvalidPool",
+        );
+      });
+  
+      it("should revert when registering a pool by a non-owner", async () => {
+        expect(
+          officialPoolsRegister
+            .connect(otherSigners[0])
+            .registerOfficialPool(poolId),
+        ).to.be.revertedWithCustomError(
+          officialPoolsRegister,
+          "Ownable__CallerIsNotOwner",
+        );
+      });
+    })
   });
 
-  describe("deregisterOfficialPool", async function () {
-    it("should emit event", async () => {
-      expect(officialPoolsRegister.deregisterOfficialPool(0))
-        .to.emit(officialPoolsRegister, "OfficalPoolDeregistered")
-        .withArgs(await owner.getAddress(), poolId);
-    });
+  describe("#deregisterOfficialPool", async function () {
+    describe("success", async () => {
+      it("should emit event", async () => {
+        expect(officialPoolsRegister.deregisterOfficialPool(0))
+          .to.emit(officialPoolsRegister, "OfficalPoolDeregistered")
+          .withArgs(await owner.getAddress(), poolId);
 
-    it("should revert when deregistering an invalid pool", async () => {
-      expect(
-        officialPoolsRegister.deregisterOfficialPool(5),
-      ).to.be.revertedWithCustomError(
-        officialPoolsRegister,
-        "OfficialPoolsRegister_InvalidPool",
-      );
-    });
 
-    it("should revert when deregistering a pool by a non-owner", () => {
-      expect(
-        officialPoolsRegister
-          .connect(otherSigners[0])
-          .deregisterOfficialPool(0),
-      ).to.be.revertedWithCustomError(
-        officialPoolsRegister,
-        "Ownable__CallerIsNotOwner",
-      );
-    });
+        const officialPoolsIds =
+          await officialPoolsRegister.getAllOfficialPools();
+        expect(officialPoolsIds[0], undefined);
+      });
+    })
+
+    describe("failure", async () => {
+      it("should revert when deregistering an invalid pool", async () => {
+        expect(
+          officialPoolsRegister.deregisterOfficialPool(5),
+        ).to.be.revertedWithCustomError(
+          officialPoolsRegister,
+          "OfficialPoolsRegister_InvalidPool",
+        );
+      });
+  
+      it("should revert when deregistering a pool by a non-owner", () => {
+        expect(
+          officialPoolsRegister
+            .connect(otherSigners[0])
+            .deregisterOfficialPool(0),
+        ).to.be.revertedWithCustomError(
+          officialPoolsRegister,
+          "Ownable__CallerIsNotOwner",
+        );
+      });
+    })
   });
 });
