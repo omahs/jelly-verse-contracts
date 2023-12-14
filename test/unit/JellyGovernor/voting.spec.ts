ed357852b17894d609b97fc7df1413ed33b6f2fd
Kaliman <akalimancevic@gmail.com>
2023-12-14 10:22:37 +0100

    test: unit for castVoteWithReasonAndParamsBySig

--- test/unit/JellyGovernor/voting.spec.ts
+++ test/unit/JellyGovernor/voting.spec.ts
@@ -307,7 +307,7 @@ export function shouldVoteOnProposals(): void {
 					proposalId,
 					this.signers.alice.address
 				);
-				console.log(hasVoted);
+
 				assert(hasVoted === true, 'Incorrect hasVoted value');
 			});
 			
@@ -338,6 +338,222 @@ export function shouldVoteOnProposals(): void {
 						this.signers.bob.address,
 						proposalId,
 						0,
+						1000,
+						proposalReason,
+						proposalParams
+					);
+			});
+		});
+	});
+
+	describe('#castVoteWithReasonAndParamsBySig:', async function () {
+		const amountToMint = utils.parseEther('100');
+		const proposalDescription = 'Test Proposal #1: Mint 100 JLY tokens to Alice';
+		const proposalReason = 'I like this proposal.';
+		let mintFunctionCalldata: string;
+		let proposalParams: string;
+		let proposalId: BigNumber;
+		let chestIDs: number[];
+		let support: number;
+		let v: number;
+		let r: string;
+		let s: string;
+
+		beforeEach(async function () {
+			mintFunctionCalldata =
+				this.mocks.mockJellyToken.interface.encodeFunctionData('mint', [
+					this.signers.alice.address,
+					amountToMint,
+				]);
+
+			proposalId = await this.jellyGovernor.callStatic.propose(
+				[this.mocks.mockJellyToken.address],
+				[amountToMint],
+				[mintFunctionCalldata],
+				proposalDescription
+			);
+
+			await this.jellyGovernor.propose(
+				[this.mocks.mockJellyToken.address],
+				[amountToMint],
+				[mintFunctionCalldata],
+				proposalDescription
+			);
+
+			chestIDs = [1, 2, 3];
+
+			proposalParams = utils.defaultAbiCoder.encode(
+			['uint256[]'],
+			[chestIDs]     
+			);
+
+			support = 1;    // example support value
+			const domain = {
+				name: 'JellyGovernor',
+				version: '1',
+				chainId: network.config.chainId, // Using the chainId from Hardhat config
+				verifyingContract: this.jellyGovernor.address // The deployed contract address
+			  };
+
+			const ballotType = {
+				ExtendedBallot: [
+					{ name: 'proposalId', type: 'uint256' },
+					{ name: 'support', type: 'uint8' },
+					{ name: 'reason', type: 'string' },
+					{ name: 'params', type: 'bytes' },
+				]
+			};
+
+			const message = {
+				proposalId,
+				support,
+				reason: proposalReason,
+				params: proposalParams,
+			};
+
+			const signature = await this.signers.deployer._signTypedData(domain, ballotType, message);
+			const splitSignature = utils.splitSignature(signature);
+			v = splitSignature.v;
+			r = splitSignature.r;
+			s = splitSignature.s;
+		});
+
+		describe('failure', async function () {
+			it('should revert if voting didn\'t start', async function () {
+				await expect(
+					this.jellyGovernor.castVoteWithReasonAndParamsBySig(proposalId, support, proposalReason, proposalParams, v, r, s)
+				).to.be.revertedWith('Governor: vote not currently active');
+			});
+
+			it('should revert if v is invalid', async function () {
+				await expect(
+					this.jellyGovernor.castVoteWithReasonAndParamsBySig(proposalId, support, proposalReason, proposalParams, 8, r, s)
+				).to.be.revertedWith('ECDSA: invalid signature');
+			});
+
+			it('should revert if r or s is invalid', async function () {
+				await expect(
+					this.jellyGovernor.castVoteWithReasonAndParamsBySig(proposalId, support, proposalReason, proposalParams, v, '0x3397dbafb3e0afdb1991626fe731e09acee7393ed317b868547ee698405d4400', s)
+				).to.be.revertedWith('ECDSA: invalid signature');
+			});
+
+			it('should revert because invalid proposal id', async function () {
+				await time.increase(this.params.votingDelay);
+				await expect(
+					this.jellyGovernor.castVoteWithReasonAndParamsBySig(proposalId.add(1000), support, proposalReason, proposalParams, v, r, s)
+				).to.be.revertedWith('Governor: unknown proposal id');
+			});
+
+			it('should revert because no chestIds in enocoded params', async function () {
+				await time.increase(this.params.votingDelay);
+				const chestIDs: number[] = [];
+				const invalidProposalParams = utils.defaultAbiCoder.encode(
+				['uint256[]'],
+				[chestIDs]     
+				);
+				await expect(
+					this.jellyGovernor.castVoteWithReasonAndParamsBySig(proposalId, support, proposalReason, invalidProposalParams, v, r, s)
+				).to.be.revertedWith('JellyGovernor: no chest IDs provided');
+			});
+
+			it('should revert because provided chestId is not viable for voting', async function () {
+				await time.increase(this.params.votingDelay);
+				const chestIDs: number[] = [10, 1];
+				const invalidProposalParams = utils.defaultAbiCoder.encode(
+				['uint256[]'],
+				[chestIDs]     
+				);
+				await expect(
+					this.jellyGovernor.castVoteWithReasonAndParamsBySig(proposalId, support, proposalReason, invalidProposalParams, v, r, s)
+				).to.be.revertedWith('JellyGovernor: chest not viable for voting');
+			});
+
+			it('should revert because provided support parameter in invalid', async function () {
+				await time.increase(this.params.votingDelay);
+				await expect(
+					this.jellyGovernor.castVoteWithReasonAndParamsBySig(proposalId, 8, proposalReason, proposalParams, v, r, s)
+				).to.be.revertedWith('GovernorVotingSimple: invalid value for enum VoteType');
+			});
+
+			it('should revert because user already voted', async function () {
+				await time.increase(this.params.votingDelay);
+				await this.jellyGovernor.castVoteWithReasonAndParamsBySig(proposalId, support, proposalReason, proposalParams, v, r, s)
+
+				await expect(
+					this.jellyGovernor.castVoteWithReasonAndParamsBySig(proposalId, support, proposalReason, proposalParams, v, r, s)
+				).to.be.revertedWith('GovernorVotingSimple: vote already cast');
+			});
+		});
+
+		describe('success', async function () {
+			beforeEach(async function () {
+				await time.increase(this.params.votingDelay);
+				await this.jellyGovernor.castVoteWithReasonAndParamsBySig(proposalId, support, proposalReason, proposalParams, v, r, s)
+			});
+
+			it('should set deployer as user who voted for proposal', async function () {
+				const hasVoted = await this.jellyGovernor.hasVoted(
+					proposalId,
+					this.signers.deployer.address
+				);
+				assert(hasVoted === true, 'Incorrect hasVoted value');
+			});
+			
+			it('should count deployer\'s votes for proposal', async function () {
+				const votes = await this.jellyGovernor.proposalVotes(
+					proposalId
+				);
+				const deployerVotes =  1000; // mocked votingPower value on chest contract
+
+				assert(votes.againstVotes.eq(0), 'Incorrect against votes value');
+				assert(votes.abstainVotes.eq(0), 'Incorrect abstain votes value');
+				assert(votes.forVotes.eq(deployerVotes), 'Incorrect for votes value');
+			});
+
+			it('should emit VoteCastWithParams event', async function () {
+				const domain = {
+					name: 'JellyGovernor',
+					version: '1',
+					chainId: network.config.chainId, // Using the chainId from Hardhat config
+					verifyingContract: this.jellyGovernor.address // The deployed contract address
+				};
+
+				const ballotType = {
+					ExtendedBallot: [
+						{ name: 'proposalId', type: 'uint256' },
+						{ name: 'support', type: 'uint8' },
+						{ name: 'reason', type: 'string' },
+						{ name: 'params', type: 'bytes' },
+					]
+				};
+
+				const message = {
+					proposalId,
+					support,
+					reason: proposalReason,
+					params: proposalParams,
+				};
+
+				const signature = await this.signers.bob._signTypedData(domain, ballotType, message);
+				const splitSignature = utils.splitSignature(signature);
+
+				await expect(
+					this.jellyGovernor
+						.castVoteWithReasonAndParamsBySig(
+							proposalId,
+							support,
+							proposalReason, 
+							proposalParams,
+							splitSignature.v,
+							splitSignature.r,
+							splitSignature.s
+						)
+				)
+					.to.emit(this.jellyGovernor, 'VoteCastWithParams')
+					.withArgs(
+						this.signers.bob.address,
+						proposalId,
+						support,
 						1000,
 						proposalReason,
 						proposalParams
